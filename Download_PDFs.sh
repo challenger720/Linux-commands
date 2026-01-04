ff1bc04285048f7ef8a81cfa696b80e5dad2b1b4
challenger720 <127072307+challenger720@users.noreply.github.com>
2026-01-04 13:41:57 -0700

    Update Download_PDFs.sh

--- Download_PDFs.sh
+++ Download_PDFs.sh
@@ -1,6 +1,14 @@
 #Recursive PDF Downloader#
 #!/bin/bash
 # Usage: ./download_pdfs.sh https://example.com
+# This script uses Bash features to download PDFs
 
 URL=$1
+
+if [ -z "$URL" ]; then
+    echo "Error: You forgot to provide a URL!"
+    echo "Usage: ./Download_PDFs.sh https://example.com"
+    exit 1
+fi
+
 wget -r -l 1 -A pdf --no-parent --no-directories "$URL"
